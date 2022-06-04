9531523e9c8f6eb31b2d17b0e70b441f0081325c
Artyom Ilyin <ilyin@infostorm.ru>
2022-06-04 08:13:28 +0300

    add: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,20 @@
+import * as fs from 'fs/promises';
+import { access, constants } from 'node:fs';
 export const create = async () => {
-    // Write your code here 
+
+	const file = './files/fresh.txt';
+	async function writeFile(file) {access(file, constants.F_OK, (err) => {
+		if (err) {
+			try {
+		fs.writeFile(file, "I am fresh and young", 'utf8')
+	} catch (error) {
+		console.error('FS operation failed');
+	}
+
+		} else {console.error('FS operation failed');}
+	});}
+
+	writeFile(file)
+
 };
+create()
