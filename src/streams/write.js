0b814639c85934ed0d2b662025e351824f66b96d
Artyom Ilyin <ilyin@infostorm.ru>
2022-06-05 09:32:16 +0300

    add: streams/transform.js adn write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 export const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const fileName = join(__dirname, 'files/fileToWrite.txt');
+
+    const writingStream = fs.createWriteStream(fileName, { flags: 'a' });
+    process.stdin.on('data', (chunk) => {
+        writingStream.write(chunk);
+    });
 };
+
+write();
