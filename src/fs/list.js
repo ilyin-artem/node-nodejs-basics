f7e1ed4b8bfa31e19226e5d84a2ed06d5ef58e7c
Artyom Ilyin <ilyin@infostorm.ru>
2022-06-05 07:57:09 +0300

    add: list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,31 @@
+import { access, readdir } from 'fs/promises';
+import { constants } from 'node:fs';
+
 export const list = async () => {
-    // Write your code here 
+    const dirName = './files/';
+
+    const listDir = async (dirName) => {
+        try {
+            if (await checkFileExists(dirName)) {
+                let data;
+                data = await readdir(dirName);
+                data.forEach((file) => {
+                    console.log(file);
+                });
+            } else {
+                throw Error('FS operation failed');
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    const checkFileExists = async (dirName) => {
+        return await access(dirName, constants.F_OK)
+            .then(() => true)
+            .catch(() => false);
+    };
+
+    listDir(dirName);
 };
+list();
