dbc4e7c7e74409af1986ecf4dea4f5b6f51ee7a0
Artyom Ilyin <ilyin@infostorm.ru>
2022-06-05 09:09:04 +0300

    add: streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,13 +1,16 @@
 import * as fs from 'fs';
-import * as path from 'path';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 export const read = async () => {
-    currentFile = path.join(__dirname, 'text.txt');
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const currentFile = join(__dirname, 'files/fileToRead.txt');
 
     const stream = fs.createReadStream(currentFile, 'utf-8');
     let data = '';
     stream.on('data', (chunk) => (data += chunk));
-    stream.on('end', () => console.log(data));
-    stream.on('error', (error) => console.log('Error', error.message));
+    stream.on('end', () => process.stdout.write(data));
+    stream.on('error', (error) => process.stdout.write('Error', error.message));
 };
 
 read();
