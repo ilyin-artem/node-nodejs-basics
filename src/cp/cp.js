2b93b887f2d0a2c049e390abb70c7ed9f76fe0b4
Artyom Ilyin <ilyin@infostorm.ru>
2022-06-05 10:44:14 +0300

    add: cp/cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import { spawn } from 'child_process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const scriptFile = join(__dirname, 'files', 'script.js');
+    const childProcess = spawn('node', [scriptFile, args]);
+
+    childProcess.stdout.pipe(process.stdout);
+    process.stdin.pipe(childProcess.stdin);
 };
+
+spawnChildProcess(process.argv);
